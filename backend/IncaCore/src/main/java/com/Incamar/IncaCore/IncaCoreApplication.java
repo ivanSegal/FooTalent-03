3e11ea606cba57e3191249339b9d0d3d6fb187a1
Iván Segal <ivaan.segal@gmail.com>
2025-07-29 21:10:25 -0300

    Subido archivos backend

---
+++ backend/IncaCore/src/main/java/com/Incamar/IncaCore/IncaCoreApplication.java
@@ -0,0 +1,13 @@
+package com.Incamar.IncaCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncaCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncaCoreApplication.class, args);
+	}
+
+}
---
+++ backend/IncaCore/src/test/java/com/Incamar/IncaCore/IncaCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Incamar.IncaCore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncaCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
