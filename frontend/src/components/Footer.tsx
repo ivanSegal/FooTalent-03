44177c88d36b91a6b032433868cd84f43fd573c0
Luis Angel <angel.quispe.navarro@outlook.com>
2025-08-03 23:38:17 -0500

    Update view fucntionality

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="z-10 flex flex-wrap items-center justify-center gap-6 rounded-t-2xl bg-white pb-6 pt-10 font-medium text-[#0b1839]">
-      <span>Prueba de Concepto</span>
+    <footer className="z-10 flex flex-wrap items-center justify-center gap-6 rounded-t-2xl bg-white pt-10 pb-6 font-medium text-[#0b1839]">
+      <span>Pruebas de Concepto</span>
       <span>Foo Talent Group</span>
       <span>Equipo 3 - Turno Noche</span>
     </footer>
