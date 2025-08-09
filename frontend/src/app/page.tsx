1f9d11b8af1b2559256f392cc52bbb1b0ca3307f
Luis Angel <angel.quispe.navarro@outlook.com>
2025-08-08 23:22:19 -0500

    Update Page Principal

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,6 @@
-import fondo from "@/assets/images/fondo.png";
-
 export default function Home() {
   return (
-    <div
-      className="relative grid min-h-screen grid-rows-[20px_auto_20px] items-start justify-items-center bg-cover bg-center p-8 sm:p-20"
-      style={{ backgroundImage: `url(${fondo.src})` }}
-    >
+    <div className="relative grid min-h-screen grid-rows-[20px_auto_20px] items-start justify-items-center bg-cover bg-center p-8 sm:p-20">
       <main className="z-10 row-start-2 -mt-12"></main>
     </div>
   );
