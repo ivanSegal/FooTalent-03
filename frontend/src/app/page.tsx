bcecba9838a7170a9912f6d1cab4d1ff41f0b5c2
Luis Angel <angel.quispe.navarro@outlook.com>
2025-08-08 23:17:41 -0500

    Clear page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,3 @@
-import ListaUsuario from "@/components/ListaUsuario";
 import fondo from "@/assets/images/fondo.png";
 
 export default function Home() {
@@ -7,9 +6,7 @@ export default function Home() {
       className="relative grid min-h-screen grid-rows-[20px_auto_20px] items-start justify-items-center bg-cover bg-center p-8 sm:p-20"
       style={{ backgroundImage: `url(${fondo.src})` }}
     >
-      <main className="z-10 row-start-2 -mt-12">
-        <ListaUsuario />
-      </main>
+      <main className="z-10 row-start-2 -mt-12"></main>
     </div>
   );
 }
