b28f6c55cbb4a63ad4ef2a41419e86b156ad0dae
Nath1710 <nathalyhernandez815@gmail.com>
2025-08-19 00:23:25 -0500

    implementar funcionalidad de colapso del sidebar y actualización de iconos en la vista principal

--- frontend/src/types/sidebar_props.ts
+++ frontend/src/types/sidebar_props.ts
@@ -2,7 +2,7 @@ import { MenuItem } from "@/types/menu_Item";
 
 export interface SidebarProps {
   title?: string; 
-  activeItemId: string;
+  activeItemId?: string;
   onItemClick?: (item: MenuItem) => void;
   onLogout?: () => void; 
   collapsed?: boolean;
