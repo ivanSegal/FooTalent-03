0006f797542526b27dd4bd3569c0decf21f0bed8
spimentel1201 <spimentelm1201@gmail.com>
2025-08-20 08:02:16 -0500

    embarcaciones modelos y dtos actualizados

--- backend/IncaCore/src/main/java/com/Incamar/IncaCore/models/Embarcacion.java
+++ backend/IncaCore/src/main/java/com/Incamar/IncaCore/models/Embarcacion.java
@@ -18,12 +18,29 @@ public class Embarcacion {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotBlank(message = "El nombre es obligatorio")
-    private String nombre;
-    @NotBlank(message = "La patente es obligatoria")
-    private String nPatente;
-    @NotBlank(message = "El capitán es obligatorio")
-    private String capitan;
-
+    private String name;
+    @NotBlank(message = "El número de matrícula es obligatoria")
+    private String vessel_registration_number;
     @NotBlank(message = "El modelo es obligatorio")
-    private String modelo;
+    private String model;
+    @NotBlank(message = "El ISSM es obligatorio")
+    private String issm;
+    @NotBlank(message = "La bandera es obligatorio")
+    private String flag;
+    @NotBlank(message = "El distintivo es obligatorio")
+    private String distinctive;
+    @NotBlank(message = "El puerto registrado es obligatorio")
+    private String registrationPort;
+    @NotBlank(message = "El RIF es obligatorio")
+    private String rif;
+    @NotBlank(message = "El uso de la embarcación es obligatorio")
+    private String use;
+    @NotBlank(message = "El material de construcción es obligatorio")
+    private String hullMaterial;
+    @NotBlank(message = "La información de la popa es obligatoria")
+    private String stern;
+    @NotBlank(message = "La información del tipo de combustible es obligatoria")
+    private String fuelType;
+
+    private String engineHours;
 }
