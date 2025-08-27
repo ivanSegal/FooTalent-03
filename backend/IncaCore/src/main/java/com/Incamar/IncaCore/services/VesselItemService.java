92cd7ca9d88bb51198a9e0425734329b3a1feeac
EdgarCamberos1894 <edgarcamberos18@gmail.com>
2025-08-26 17:01:15 -0700

    feat(vesselItemsHours) Añadir funcionalidad para añadir hora a items de mantenimiento

--- backend/IncaCore/src/main/java/com/Incamar/IncaCore/services/VesselItemService.java
+++ backend/IncaCore/src/main/java/com/Incamar/IncaCore/services/VesselItemService.java
@@ -2,6 +2,7 @@ package com.Incamar.IncaCore.services;
 
 import com.Incamar.IncaCore.dtos.vesselItem.VesselItemReq;
 import com.Incamar.IncaCore.dtos.vesselItem.VesselItemRes;
+import com.Incamar.IncaCore.dtos.vesselItem.VesselItemSearchReq;
 import com.Incamar.IncaCore.dtos.vesselItem.VesselItemUpdateReq;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,7 +10,7 @@ import org.springframework.data.domain.Pageable;
 
 public interface VesselItemService {
 
-    Page<VesselItemRes> getAll(Pageable pageable);
+    Page<VesselItemRes> getAllWithSearch(VesselItemSearchReq request, Pageable pageable);
 
     VesselItemRes getById(Long id);
 
